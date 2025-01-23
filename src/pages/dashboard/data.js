278f584b86ec56a6ce2f4d00496ad8dc4fdc1682
Samuel <Samueladeyemi2006@gmail.com>
2025-01-23 10:38:07 +0100

    feat: submission page done

---
+++ src/pages/dashboard/data.js
@@ -0,0 +1,22 @@
+export const submissionData = [
+  {
+    submissionId: 30384,
+    date: "21-01-2025",
+    status: "pending",
+  },
+  {
+    submissionId: 30384,
+    date: "21-01-2025",
+    status: "Approved",
+  },
+  {
+    submissionId: 30384,
+    date: "21-01-2025",
+    status: "pending",
+  },
+  {
+    submissionId: 30384,
+    date: "21-01-2025",
+    status: "Rejected",
+  },
+];
--- src/pages/dashboard/submissions.jsx
+++ src/pages/dashboard/submissions.jsx
@@ -1,5 +1,34 @@
+import { submissionData } from "./data";
+
 function Submission() {
-  return <div>Submission</div>;
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <h2 className="text-2xl font-bold mb-4">Submissions Management</h2>
+      <div className="overflow-x-auto">
+        <div className="min-w-[600px]">
+          <div className="grid grid-cols-4 gap-4 bg-gray-200 p-4 rounded-t-lg">
+            <h2 className="text-lg font-semibold">Submission Id</h2>
+            <h2 className="text-lg font-semibold">Date</h2>
+            <h2 className="text-lg font-semibold">Status</h2>
+            <h2 className="text-lg font-semibold">Actions</h2>
+          </div>
+          {submissionData.map((item) => (
+            <div
+              key={item.submissionId}
+              className="grid grid-cols-4 gap-4 bg-white p-4 border-b border-gray-200"
+            >
+              <h2>#{item.submissionId}</h2>
+              <h2>{item.date}</h2>
+              <h2>{item.status}</h2>
+              <button className="p-2 w-fit text-white rounded-lg bg-[#3AAE9F]">
+                View
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Submission;
